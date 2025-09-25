14e1e08f8eaed60cdbc9838ce0e349b5b62e2768
공세민 <154436207+SeMinKong@users.noreply.github.com>
2025-09-25 16:13:35 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n), dp(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    dp[0] = arr[0];
+    int max_sum = dp[0];
+
+    for (int i = 1; i < n; i++) {
+        dp[i] = max(arr[i], dp[i - 1] + arr[i]);
+        max_sum = max(max_sum, dp[i]);
+    }
+
+    cout << max_sum;
+
+    return 0;
+}
