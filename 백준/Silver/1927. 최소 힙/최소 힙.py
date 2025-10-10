7a7d772c4c1c25c497e0d10d7f906a11fa47a700
공세민 <154436207+SeMinKong@users.noreply.github.com>
2025-10-10 19:18:05 +0900

    [Silver II] Title: 최소 힙, Time: 100 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,22 @@
+import heapq
+class Solve:
+    def __init__(self):
+        pass
+
+    def solve(self):
+        import sys
+        input = sys.stdin.readline
+
+        N = int(input().strip())
+        x = []
+        for _ in range(N):
+            num = int(input().strip())
+            if len(x) == 0 and num == 0:
+                print(0)
+            elif num == 0:
+                print(heapq.heappop(x))
+            else:
+                heapq.heappush(x, num)
+
+if __name__ == "__main__":
+    Solve().solve()
