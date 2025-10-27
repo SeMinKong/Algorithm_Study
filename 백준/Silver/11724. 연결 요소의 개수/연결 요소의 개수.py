6db0aed423fd280c07b1c49e2a33421322ae7fda
공세민 <154436207+SeMinKong@users.noreply.github.com>
2025-10-27 19:31:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 452 ms, Memory: 39196 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,39 @@
-import sys
 
-def dfs(u):
-    stack = [u]
-    visited[u] = True
+class Solve:
+    def __init__(self):
+        pass
 
-    while stack:
-        u = stack.pop()
+    def dfs(self, visited, graph, u):
+        stack = [u]
+        visited[u] = True
+
+        while stack:
+            u = stack.pop()
+            for i in range(1, len(visited)):
+                if graph[u][i] == 1 and visited[i] == False:
+                    visited[i] = True
+                    stack.append(i)
+
+    def solve(self):
+        import sys
+        input = sys.stdin.readline
+
+        N, M = list(map(int, input().split()))
+
+        graph = [[0] * (N+1) for _ in range(N+1)]
+        visited = [False] * (N+1)
+
+        for _ in range(M):
+            u, v = list(map(int, input().split()))
+            graph[u][v] = graph[v][u] = 1
+
+        ans = 0
+    
         for i in range(1, N+1):
-            if graph[u][i] == 1 and visited[i] == False:
-                visited[i] = True
-                stack.append(i)
-
-input = sys.stdin.readline
-
-N, M = list(map(int, input().split()))
-
-graph = [[0] * (N+1) for _ in range(N+1)]
-visited = [False] * (N+1)
-
-for _ in range(M):
-    u, v = list(map(int, input().split()))
-    graph[u][v] = graph[v][u] = 1
-
-ans = 0
-
-for i in range(1, N+1):
-    if visited[i] == False :
-        dfs(i)
-        ans += 1
-
-print(ans)
+            if visited[i] == False:
+                self.dfs(visited, graph, i)
+                ans += 1
+        
+        print(ans)
+if __name__ == "__main__":
+    Solve().solve()
