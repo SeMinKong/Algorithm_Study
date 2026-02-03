9db6b452c47500ca9fec7e6f8f086176f5134627
공세민 <154436207+SeMinKong@users.noreply.github.com>
2026-02-03 14:04:23 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int path[9], input[9];
+bool visited[10001];
+int N, M;
+
+void func(int lev)
+{
+	if (lev == M + 1)
+	{
+		for (int i = 1; i <= M; i++) {
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (visited[input[i - 1]] || path[lev - 1] >= input[i - 1]) continue;
+		path[lev] = input[i - 1];
+		visited[input[i - 1]] = true;
+		func(lev + 1);
+		path[lev] = 0;
+		visited[input[i - 1]] = false;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> input[i];
+	}
+
+	sort(input, input + N);
+
+	func(1);
+	return 0;
+}
